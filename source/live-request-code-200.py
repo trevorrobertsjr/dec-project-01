2eac383e9977fe5270795faec4b90b5f14411956
Max Gregory <gregoryandmax@gmail.com>
2023-08-29 16:13:48 -0400

    added file for filtering check

--- source/live-request-code-200.py
+++ source/live-request-code-200.py
@@ -11,16 +11,16 @@ import pandas as pd
 if __name__ == "__main__":
     # Load environment data from .env
     load_dotenv()
-    # zipcode_list_2 = ["35649", "35749"]
-    zipcode_list_2 = ["10003"]
+    zipcode_list_2 = ["35649", "35749"]
+   # zipcode_list_2 = ["10003"]
 
-    #for loop
+    # For loop
     for zipcode in zipcode_list_2:
         
         # API endpoint
         url = "https://us-real-estate.p.rapidapi.com/v2/sold-homes-by-zipcode"
 
-    # need to create for loop. Need to use variable for zipcode.
+    # Variable for zipcode
         querystring = {"zipcode":zipcode,"offset":"0","limit":"42"}
 
         headers = {
@@ -32,17 +32,16 @@ if __name__ == "__main__":
         # print(response.json())
 
         # Create a dictionary from JSON file with the home search results only
-        filtered = response.json()['data']['home_search']
-        # print(filtered)
+        if response.status_code == 200:
+            filtered = response.json()['data']['home_search']
+            # Check if ‘results’ exists and is not empty
+            if 'results' in filtered and filtered['results']:
+                # Flatten nested JSON
+                df = pd.json_normalize(filtered, record_path=['results'])
 
-        # # Flatten nested JSON
-        df = pd.json_normalize(filtered, record_path=['results'])
-        # print(df)
-
-        # # Create a new dataframe from only the interesting columns
+        # Create a new dataframe from only the interesting columns
         df_filtered = df[['permalink','list_price','list_date','description.sold_date', 'location.address.postal_code','location.county.name','location.address.city','location.address.state', 'description.sqft', 'description.lot_sqft']]
+        
+        # Write command to write df_filtered to a file (this will be the 300 jsons)
         df_filtered.to_json(r"output-data/"+zipcode+".json")
-
-    # need to write command to write df_filtered to a file (this will be the 300 jsons)
-
     
