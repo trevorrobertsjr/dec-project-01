1e77d12f1dd38ecde5dfd489ec1b3cffb2ff1ec4
Trevor Roberts Jr <Trevor.Roberts.Jr@gmail.com>
2023-09-01 17:06:57 -0400

    updated elt filename, refactored raw db init as elt function

---
+++ source/us_real_estate/assets/sql/extract/us_real_estate.sql
@@ -0,0 +1,23 @@
+{% set config = {
+    "extract_type": "incremental", 
+    "incremental_column": "description_sold_date",
+    "source_table_name": "us_real_estate_listings"
+} %}
+
+select 
+    permalink,
+    list_price,
+    list_date,
+    description_sold_date,
+    location_address_postal_code,
+    location_county_name,
+    location_address_city,
+    location_address_state,
+    description_sqft,
+    description_lot_sqft
+from 
+    {{ config["source_table_name"] }}
+
+{% if is_incremental %}
+    where {{ config["incremental_column"] }} > '{{ incremental_value }}'
+{% endif %}
--- source/us_real_estate/connectors/postgres.py
+++ source/us_real_estate/connectors/postgres.py
@@ -1,8 +1,7 @@
-from sqlalchemy import create_engine, Table, MetaData
+from sqlalchemy import create_engine, Table, MetaData, Column, inspect
 from sqlalchemy.engine import URL
 from sqlalchemy.dialects import postgresql
-from sqlalchemy import create_engine, Table, MetaData
-from sqlalchemy.dialects import postgresql
+
 
 class PostgreSqlClient:
     """
@@ -31,7 +30,7 @@ class PostgreSqlClient:
         )
 
         self.engine = create_engine(connection_url)
-
+    
     def write_to_table(self, data: list[dict], table: Table, metadata: MetaData) -> None:
         key_columns = [pk_column.name for pk_column in table.primary_key.columns.values()]
         metadata.create_all(self.engine) # creates table if it does not exist 
@@ -41,3 +40,111 @@ class PostgreSqlClient:
             set_={c.key: c for c in insert_statement.excluded if c.key not in key_columns})
         self.engine.execute(upsert_statement)
 
+    def execute_sql(self, sql: str) -> None:
+        self.engine.execute(sql)
+
+    def select_all(self, table: Table)-> list[dict]:
+        """
+        Execute SQL code provided and returns the result in a list of dictionaries. 
+        This method should only be used if you expect a resultset to be returned. 
+        """
+        return [dict(row) for row in self.engine.execute(table.select()).all()]
+
+    def run_sql(self, sql: str)-> list[dict]:
+        """
+        Execute SQL code provided and returns the result in a list of dictionaries. 
+        This method should only be used if you expect a resultset to be returned. 
+        """
+        return [dict(row) for row in self.engine.execute(sql).all()]
+
+    def get_metadata(self) -> MetaData:
+        """
+        Gets the metadata object for all tables for a given database 
+        """
+        metadata = MetaData(bind=self.engine)
+        metadata.reflect()
+        return metadata
+
+    def get_table_schema(self, table_name: str) -> Table:
+        """
+        Gets the table schema and metadata 
+        """
+        metadata = self.get_metadata()
+        return metadata.tables[table_name], metadata
+
+    def table_exists(self, table_name: str) -> bool: 
+        """
+        Checks if the table already exists in the database. 
+        """
+        return inspect(self.engine).has_table(table_name)
+
+    def create_table(self, table_name: str, metadata: MetaData) -> None: 
+        """
+        Creates a single table provided in the metadata object
+        """
+        existing_table = metadata.tables[table_name]
+        new_metadata = MetaData()
+        columns = [Column(column.name, 
+                          column.type, 
+                          primary_key=column.primary_key
+            ) for column in existing_table.columns]
+        new_table = Table(
+            table_name, 
+            new_metadata,
+            *columns    
+        )
+        new_metadata.create_all(bind = self.engine)
+
+    def create_all_tables(self, metadata: MetaData) -> None:
+        """
+        Creates tables provided in the metadata object
+        """
+        metadata.create_all(self.engine)
+    
+    def drop_table(self, table_name: str) -> None: 
+        """
+        Drops a specified table if it exists
+        """
+        self.engine.execute(f"drop table if exists {table_name};")
+    
+    def insert(self, data: list[dict], table: Table, metadata: MetaData) -> None:
+        """
+        Insert data into a database table. This method creates the table also if it doesn't exist. 
+        """
+        self.create_table(table_name=table.name, metadata=metadata)
+        insert_statement = postgresql.insert(table).values(data)
+        self.engine.execute(insert_statement)
+    
+    def overwrite(self, data: list[dict], table: Table, metadata: MetaData) -> None: 
+        """
+        Overwrites data into a database table. This method creates the table also if it doesn't exist. 
+        """
+        self.drop_table(table.name)
+        self.insert(data=data, table=table, metadata=metadata)
+
+    def upsert(self, data: list[dict], table: Table, metadata: MetaData) -> None:
+        """
+        Upserts data into a database table. This method creates the table also if it doesn't exist. 
+        """
+        self.create_table(table_name=table.name, metadata=metadata)
+        key_columns = [pk_column.name for pk_column in table.primary_key.columns.values()]
+        insert_statement = postgresql.insert(table).values(data)
+        upsert_statement = insert_statement.on_conflict_do_update(
+            index_elements=key_columns,
+            set_={c.key: c for c in insert_statement.excluded if c.key not in key_columns})
+        self.engine.execute(upsert_statement)
+
+    def upsert_in_chunks(self, data: list[dict], table: Table, metadata: MetaData, chunksize: int = 1000) -> None: 
+        """
+        Upserts data into a database table in chunks (e.g. 1000 rows at a time) in case of query timeouts or row limitations. 
+        This method creates the table also if it doesn't exist. 
+        """
+        max_length = len(data)
+        for i in range(0, max_length, chunksize):
+            if i + chunksize >= max_length: 
+                lower_bound = i
+                upper_bound = max_length 
+            else: 
+                lower_bound = i 
+                upper_bound = i + chunksize
+            self.upsert(data = data[lower_bound:upper_bound], table = table, metadata = metadata)
